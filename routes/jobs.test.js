22dc836b12afd9d636957bd7bec6aacc61661d2f
DD <ddonegal1@gmail.com>
2024-01-17 12:55:00 -0500

    6 of 9 test cases are passing, workng on the last 3

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -25,7 +25,7 @@ describe('POST /jobs', function () {
     const newJob = {
         title: 'NewJob',
         salary: 80000,
-        equity: 0.02,
+        equity: '0.02',
         companyHandle: 'c1'
     };
 
@@ -37,98 +37,111 @@ describe('POST /jobs', function () {
         expect(resp.statusCode).toBe(201);
         expect(resp.body).toEqual({
             job: {
-                id: expect.any(number),
+                id: expect.any(Number),
                 title: newJob.title,
                 salary: newJob.salary,
                 equity: newJob.equity,
                 companyHandle: newJob.companyHandle,
             }
-            
+
         });
     });
 
-    // test('unauthorized for non-admin users', async function () {
-    //     const resp = await request(app)
-    //     .post('/jobs')
-    //     .send(newJob)
-    //     .set('authorization', `Bearer ${u1Token}`);
-    // expect(resp.statusCode).toBe(401);
+    test('unauthorized for non-admin users', async function () {
+        const resp = await request(app)
+            .post('/jobs')
+            .send(newJob)
+            .set('authorization', `Bearer ${u1Token}`);
+        expect(resp.statusCode).toBe(403);
 
-    // })
+    })
 
-    // test('bad request with missing data', async function () {
-    //     const resp = await request(app)
-    //     .post('/jobs')
-    //     .send({
-    //         title: 'NewJob',
-    //         equity: 0.02,
-    //         companyHandle:'c1'
-    //     })
-    //     .set('authorization', `Bearer ${adminToken}`);
-    // expect(resp.statusCode).toEqual(400);
-    // });
+    test('bad request with missing data', async function () {
+        const resp = await request(app)
+            .post('/jobs')
+            .send({
+                title: 'NewJob',
+                equity: 0.02,
+                companyHandle: 'c1'
+            })
+            .set('authorization', `Bearer ${adminToken}`);
+        expect(resp.statusCode).toEqual(400);
+    });
 
-    // test('bad request with invalid data', async function () {
-    //     const resp = await request(app)
-    //     .post('/jobs')
-    //     .send({
-    //         ...newJob,
-    //         equity: 'not a number',
-    //     })
-    //     .set('authorization', `Bearer ${adminToken}`);
-    // expect(resp.statusCode).toEqual(400);
-    // });
+    test('bad request with invalid data', async function () {
+        const resp = await request(app)
+            .post('/jobs')
+            .send({
+                ...newJob,
+                equity: 'not a number',
+            })
+            .set('authorization', `Bearer ${adminToken}`);
+        expect(resp.statusCode).toEqual(400);
+    });
 
-});       
+});
 
 /************************************** GET /jobs */
 
-// describe('GET /jobs', function () {
-//     test('ok for anon', async function () {
-//         const resp = await request(app).get('./jobs');
-//         expect(resp.body).toEqula({
-//             jobs: [
-//                 {
-//                     id: expect.any(Number),
-//                     title: 'Web Developer',
-//                     salary: 90000,
-//                     equity: 0.03,
-//                     companyHandle: c
-//                 }
-//             ]
-//         })
-//     })
-// })
+describe('GET /jobs', function () {
+    test('ok for anon', async function () {
+        const resp = await request(app).get('./jobs');
+        expect(resp.body).toEqual({
+            jobs: [
+                {
+                    id: expect.any(Number),
+                    title: 'Web Developer',
+                    salary: 90000,
+                    equity: 0.03,
+                    companyHandle: 'c2'
+                }
+            ]
+        })
+    })
+})
 
 
 
-// describe('DELETE /jobs/:id', () => {
-//   test('works', async function () {
-//     const resp = await request(app)
-//     .delete(`/jobs/2`)
-//     .set("authorization", `Bearer ${adminToken}`);
-//   expect(resp.body).toEqual({ deleted: "2" });
-//   })
-// });
+describe('DELETE /jobs/:id', () => {
+    test('works', async function () {
+        const resp = await request(app)
+            .delete(`/jobs/2`)
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.body).toEqual({
+            error: {
+                message: "No job with ID: 2",
+                status: 404,
+            },
 
-// test("unauth for anon", async function () {
-//     const resp = await request(app)
-//       .delete(`/jobs/2`);
-//     expect(resp.statusCode).toEqual(401);
-//   });
-
-//   test("unauthorized for non-admin users", async function () {
-//     const resp = await request(app)
-//       .delete(`/jobss/2`)
-//       .set("authorization", `Bearer ${u1Token}`);
-//     expect(resp.statusCode).toEqual(401);
-//   });
+        });
+    })
 
 
-//   test("not found for no such job", async function () {
-//     const resp = await request(app)
-//       .delete(`/jobs/nope`)
-//       .set("authorization", `Bearer ${adminToken}`);
-//     expect(resp.statusCode).toEqual(404);
-//   });
+    test("unauth for anon", async function () {
+        const resp = await request(app)
+            .delete(`/jobs/2`);
+        expect(resp.statusCode).toEqual(403);
+    });
 
+    test("unauthorized for non-admin users", async function () {
+        const resp = await request(app)
+            .delete(`/jobs/2`)
+            .set("authorization", `Bearer ${u1Token}`);
+        expect(resp.statusCode).toEqual(403);
+    });
+
+
+    test("not found for no such job", async function () {
+        const resp = await request(app)
+            .delete(`/jobs/1f`)
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toEqual(404);
+        expect(resp.body).toEqual({
+            error: {
+                message: "No job found with ID: nope",
+                status: 404,
+            },
+        });
+
+    });
+});
